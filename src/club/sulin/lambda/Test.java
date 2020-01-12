951b22c039ef01ee920240089e1dfb480aa78d4f
Sulin <1848424208@qq.com>
2020-01-12 22:57:19 +0800

    slc add and commit /lambda/Test/m2

--- src/club/sulin/lambda/Test.java
+++ src/club/sulin/lambda/Test.java
@@ -21,6 +21,11 @@ public class Test {
 
 
     public static void m2(int a, Consumer<Integer> consumer) {
+        System.out.println("slc add");
+        System.out.println("slc add");
+        System.out.println("slc add");
+        System.out.println("slc add");
+        System.out.println("slc add");
         consumer.accept(a);
         System.out.println("other");
     }
